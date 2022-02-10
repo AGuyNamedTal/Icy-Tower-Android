287e92ec025c78c588cfa7d92f98f5bdf913d52a
Tal V <TalTheBest204@gmail.com>
2022-02-10 16:18:16 +0200

    Work on scoreboard

---
+++ app/src/main/java/com/talv/icytower/game/platform/PlatformImage.java
@@ -0,0 +1,42 @@
+package com.talv.icytower.game.platform;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+import com.talv.icytower.ImageHelper;
+import com.talv.icytower.game.Engine;
+
+public class PlatformImage {
+
+
+    private Bitmap middleBitmap;
+    private Bitmap leftBitmap;
+    private Bitmap rightBitmap;
+
+    private int sideBitmapsWidth;
+
+    public PlatformImage(Bitmap middleBitmap, Bitmap rightBitmap) {
+        this.middleBitmap = middleBitmap;
+        this.rightBitmap = rightBitmap;
+        sideBitmapsWidth = rightBitmap.getWidth();
+        this.leftBitmap = ImageHelper.flipX(rightBitmap);
+    }
+
+    public Bitmap createPlatformImage(int width, int height) {
+        Bitmap platformBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(platformBitmap);
+        canvas.drawBitmap(leftBitmap, 0, 0, Engine.gamePaint);
+        canvas.drawBitmap(ImageHelper.tileImageX(middleBitmap, width - sideBitmapsWidth * 2, height, false),
+                sideBitmapsWidth, 0, Engine.gamePaint);
+        canvas.drawBitmap(rightBitmap, width - sideBitmapsWidth, 0, Engine.gamePaint);
+        return platformBitmap;
+    }
+
+    public void recycle() {
+        middleBitmap.recycle();
+        rightBitmap.recycle();
+        leftBitmap.recycle();
+    }
+
+
+}
